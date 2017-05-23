b3393d1d70bfd918b832c10cdef50ed03601a238
luyouxin84 <lu_youxin@hotmail.com>
2017-05-23 09:30:13 +0800

    2017年5月23日09:30:09
    sfsf

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,17 +1,11 @@
-import {
-  Component,
-  OnInit,
-  EventEmitter,
-  Output,
-  ViewChild
-} from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, ViewChild} from '@angular/core';
 import {NgModel} from "@angular/forms";
+import * as sjcl from './sjcl.js';
+import { httpHandle } from '../../common_method/http_handle';
 
-@Component({
-  selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
 })
+@Component({selector: 'app-login', templateUrl: './login.component.html', styleUrls: ['./login.component.scss']})
 export class LoginComponent implements OnInit {
   name:string;
   pass:string;
@@ -24,7 +18,29 @@ export class LoginComponent implements OnInit {
   submit(x:any){
     console.log(x);
   }
-  test(){
+  @ViewChild('pas')_pass : NgModel;
+
+  ngOnInit() {}
+  submit() {
+    console.log(sjcl['encrypt']);
+    let pass_handel = sjcl['encrypt']("password",this.pass);
+    console.log(pass_handel);
+    // it's string
+    // console.log(typeof(pass_handel));    
+    var ciphertext = sjcl['encrypt']("password", this.name + pass_handel);
+    console.log(ciphertext);
+    this._http.fetch_post('https://www.lyxsblog.cn/login_up',ciphertext)
+    .map( res => res.json())
+    .subscribe(
+      res=>{
+        console.log(res);
+      },
+      err =>{
+        console.info(err)
+      }
+    )
   }
-  
+  test() {}
+
 }
+
